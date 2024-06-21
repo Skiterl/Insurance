fc0e6bb854af3192a606f127abacf1ec7636c18d
ZharkovAI <alexzhar03@gmail.com>
2024-06-21 21:03:04 +0300

    Start of the project
    Adding the necessary dependencies (Possible addition)

---
+++ src/main/kotlin/ru/skitel/insurance/InsuranceApplication.kt
@@ -0,0 +1,11 @@
+package ru.skitel.insurance
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InsuranceApplication
+
+fun main(args: Array<String>) {
+	runApplication<InsuranceApplication>(*args)
+}
